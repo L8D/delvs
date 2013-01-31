9ecab9220c90be3f93c6c3d6948b557b0d2f471f
Michael Schrijver <michael@hating.biz>
2013-01-31 10:31:59 +0100

    - initial import

---
+++ compiler.cc
@@ -0,0 +1,34 @@
+#include "brainfuck.h"
+#include <cstring>
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	for (int i = 1 ; i < argc ; i++)
+	{
+		try
+		{
+			brainfuck::parser p;
+			ifstream is;
+			is.open(argv[i], ios::binary);
+		
+			while (!is.eof())
+			{
+				char buf[4096];
+				is.read(buf, 4096);
+				if (is.gcount() > 0)
+					brainfuck::lexer(buf, is.gcount(), p);
+			}
+			brainfuck::cout_visitor visitor;
+			p.program()->visit(visitor);
+		}
+		catch (const exception &e)
+		{
+			cerr << "Error running " << argv[i] << ": " << e.what() << endl;
+		}
+	}
+}
