1a3c5c4b619b4c32923959c517c4a2f84b18f819
L8D <tenorbiel@gmail.com>
2013-05-28 00:08:18 -0500

    Still cleaning...

--- delvs.h
+++ delvs.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-void lexer(const char *source, short int *p) {
-  for (const char *c = source; *c; c++) {
+void lexer(const char *c, short int *p) {
+  for (; *c; c++) {
     switch (*c) {
       case '>':
         ++p;
@@ -43,6 +43,7 @@ void lexer(const char *source, short int *p) {
 /*
  * Usage:
  * const char *code = ",[.,]";
+ * code = malloc(sizeof(*code));
  * short int data[30000];
  * short int *p = &data[0];
  * lexer(code, p);
