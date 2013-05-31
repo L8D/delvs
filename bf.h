c7ec520e5792346d53e47c00433bd4bbf32766e0
L8D <tenorbiel@gmail.com>
2013-05-31 18:28:55 -0500

    Created for use of minimal vanilla interpreter

---
+++ bf.h
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+const char *lexer(const char *c, char *p) {
+  const char *cc = c;
+  while(*c) {
+    switch (*c++) {
+      case '>':
+        ++p;
+        break;
+
+      case '<':
+        --p;
+        break;
+
+      case '+':
+        ++*p;
+        break;
+
+      case '-':
+        --*p;
+        break;
+
+      case '.':
+        putchar(*p);
+        break;
+
+      case ',':
+        *p = getchar();
+        break;
+
+      case '[':
+        cc = c;
+        while(*p) {
+          c = cc; // restore char position to start of loop
+          c = lexer(c + 1, p);
+        }
+        break;
+
+      case ']':
+        return c;
+
+      default:
+        // everything else is comments
+        break;
+    }
+  }
+  return c;
+}
+/*
+ * Usage:
+ * const char *code = ",[.,]";
+ * char data[30000];
+ * char *p = &data[0];
+ * lexer(&code, p);
+ */
