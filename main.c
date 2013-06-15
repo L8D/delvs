6143861cb1694abe196ee512c318766f04e87787
L8D <tenorbiel@gmail.com>
2013-06-15 18:18:29 -0500

    Better file checking and more runtime handling

--- main.c
+++ main.c
@@ -1,54 +1,45 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdbool.h>
 #include <stdlib.h>
 #include "delvs.h"
 
-bool fileExists(const char *filename) {
-  FILE *file;
-  if ((file = fopen(filename, "r")) == NULL) {
-    return false;
-  } else {
-    fclose(file);
-    return true;
-  }
-}
-
 int main(int argc, char *argv[]) {
   if(argc > 1) {
-    if(fileExists(argv[1])) {
-      FILE *fp;
-      long size;
+    FILE *fp;
+    long size;
 
-      fp = fopen(argv[1], "r");
-
-      fseek(fp, 0, SEEK_END);
-      size = ftell(fp);
-      rewind(fp);
-
-      char *temp = (char*) malloc (100);
-
-      char *file = (char*) malloc (size);
-
-      file[0] = '\0';
-
-      while (fgets(temp, 100, fp) != NULL) strcat(file, temp);
-
-      free(temp);
-      fclose(fp);
-
-      struct Data g;
-
-      g.code = file;
-      g.bit = 7;
-      g.pointer = &g.vars[15000];
-
-      lexer(&g);
+    fp = fopen(argv[1], "r");
+    
+    if(fp == NULL) {
+      printf("File: \"%s\" not found\n", argv[1]);
+      return 1;
     }
-    else {
-      printf("File not found!\n");
-    }
+
+    fseek(fp, 0, SEEK_END);
+    size = ftell(fp);
+    rewind(fp);
+
+    char *temp = (char*) malloc (100);
+
+    char *file = (char*) malloc (size);
+
+    file[0] = '\0';
+
+    while (fgets(temp, 100, fp) != NULL) strcat(file, temp);
+
+    free(temp);
+    fclose(fp);
+
+    struct Data g;
+
+    g.code = file;
+    g.bit = 7;
+    g.pointer = &g.vars[15000];
+
+    lexer(&g);
   }
-
+  else {
+    printf("Usage: %s <filename>\n", argv[0]);
+  }
   return 0;
 }
